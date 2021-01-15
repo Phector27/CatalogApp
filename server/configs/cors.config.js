cb14a0ed4c882ea563ef999297929c8a2e6e3920
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2021-01-15 13:03:44 +0100

    Server running deploy

--- server/configs/cors.config.js
+++ server/configs/cors.config.js
@@ -1,6 +1,6 @@
 const cors = require('cors')
 
-const whitelist = ['http://localhost:3000']
+const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
 
 const corsOptions = {
     origin: (origin, cb) => {
